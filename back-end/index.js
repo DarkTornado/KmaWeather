31fc4d3e5e71a6a79d4ad4a7c4185c077f294170
Dark Tornado <dt3141592@naver.com>
2022-08-18 04:07:32 +0900

    동기 처리로 변경

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,28 @@
 const http = require('http');
-http.createServer((req, res) => {
+const axios = require('axios');
+
+http.createServer(async (req, res) => {
     res.writeHead(200, {
         'Content-Type': 'application/json; charset=utf-8;'
     });
-    if (req.url.startsWith('/weather')) {
-        getWeather(res, req.url);
-    } else {
+    //파라미터로 넘어온게 없으면 아무것도 안함
+    if (!req.url.includes('?')) {
+        res.write('[]');
         res.end();
+        return;
     }
+
+    //파라미터 분리
+    const query = req.url.split('?');
+    const params = query2object(query[1]);
+
+    //날씨 API
+    if (req.url.startsWith('/weather')) {
+        const result = await getWeather(params);
+        res.write(JSON.stringify(result));
+    }
+
+    res.end();
 }).listen(8080);
 
 const query2object = (query) => {
@@ -20,35 +35,20 @@ const query2object = (query) => {
     return result;
 }
 
-const getWeather = (res, _url) => {
-    if (!_url.includes('?')) {
-        res.write('[]');
-        res.end();
-        return;
-    }
-    const query = _url.split('?');
-    const params = query2object(query[1]);
-
-    const axios = require('axios');
+const getWeather = async (params) => {
     const key = '안알랴줌';
     const url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=' + key + '&pageNo=1&numOfRows=1000&dataType=JSON&base_date=' + getDate() + '&base_time=2300&nx=' + params.x + '&ny=' + params.y;
-    axios.get(url, {}, {})
-        .then((data) => {
-            data = data.data.response.body.items.item;
-            const result = {};
-            for (let n = 0; n < data.length; n++) {
-                const datum = data[n];
-                const time = datum.fcstTime;
-                if (result[time] == undefined) result[time] = {};
-                result[time][datum.category] = datum.fcstValue;
-            }
-            parseData(result, res);
-        })
-        .catch((e) => {
-            console.log('Error: ' + e);
-            res.write('[]');
-            res.end();
-        });
+    var data = await axios.get(url, {}, {});
+    if (data.data.response.body === undefined) return [];
+    data = data.data.response.body.items.item;
+    const result = {};
+    for (let n = 0; n < data.length; n++) {
+        const datum = data[n];
+        const time = datum.fcstTime;
+        if (result[time] == undefined) result[time] = {};
+        result[time][datum.category] = datum.fcstValue;
+    }
+    return parseData(result);
 }
 
 const getDate = () => {
@@ -61,7 +61,7 @@ const getDate = () => {
     return '' + y + m + d;
 }
 
-const parseData = (data, res) => {
+const parseData = (data) => {
     const result = [];
     const skys = [null, '맑음', null, '구름많음', '흐림'];
     const rains = [null, '비', '비 또는 눈', '눈', '소나기'];
@@ -78,10 +78,8 @@ const parseData = (data, res) => {
         result[n].wind_speed = datum.WSD + 'm/s';
         result[n].rain = datum.POP + '%';
     }
-    res.write(JSON.stringify(result));
-	res.end();
+    return result;
 }
-
 
 const deg2dir = (deg) => {
     const dirs = ['북', '북동', '동', '남동', '남', '남서', '서', '북서', '북'];
