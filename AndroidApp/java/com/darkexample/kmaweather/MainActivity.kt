5deac86fd626e7dec173f7269fc1050a1c9bbd11
Dark Tornado <dt3141592@naver.com>
2022-08-23 19:55:55 +0900

    front-end init

---
+++ AndroidApp/java/com/darkexample/kmaweather/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.darkexample.kmaweather
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
