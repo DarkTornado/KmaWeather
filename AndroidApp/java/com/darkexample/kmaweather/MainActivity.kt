92a87f4b7ff8961907a30f43ebdc61a3b40a1fca
Dark Tornado <dt3141592@naver.com>
2022-08-24 20:31:21 +0900

    view init

--- AndroidApp/java/com/darkexample/kmaweather/MainActivity.kt
+++ AndroidApp/java/com/darkexample/kmaweather/MainActivity.kt
@@ -2,9 +2,13 @@ package com.darkexample.kmaweather
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val web = WebView(this)
+        web.loadUrl("file:///android_asset/hangul.html")
+        setContentView(web);
     }
 }
